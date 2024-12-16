a594d5d82d724d2084b54509cc47fc4490829c1d
Kostas Vlachos <kostas-vl@hotmail.com>
2024-12-16 19:42:28 +0200

    project created

---
+++ src/gr/aueb/cf/ch2/SubCalculator.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch2;
+
+public class SubCalculator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = 0;
+
+        result = sub(num1,num2);
+    }
+
+    public static int sub(int a, int b){
+        return a - b;
+    }
+}
